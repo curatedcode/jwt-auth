250e116b9d76ec05b67c36bb43f1bcd4741a7b18
Zack F <zackaryfotheringham@hotmail.com>
2023-02-18 05:52:49 -0500

    add ability to login and generate token

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
@@ -9,8 +10,50 @@ const jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = process.env.JWT_SECRET_KEY
 
-app.use('/', (req, res) => {
+const users = [
+  {
+    id: 1,
+    name: 'thatguyben',
+    password: 'sdfg45332498hdfv'
+  },
+  {
+    id: 2,
+    name: 'test',
+    password: 'test'
+  }
+]
+
+const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+  const user = users.findIndex( (user) => user.id === jwt_payload.id )
+  if (!user) {
+    next(null, false, { message: "Cannot find user" })
+  } else {
+    next(null, user)
+  }
+})
+
+passport.use(strategy)
+app.use(passport.initialize())
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
   res.json({ message: "Welcome to the jwt-auth API" })
+})
+
+app.post('/login', (req, res) => {
+  const userIndex = users.findIndex( user => user.name === req.body.name)
+  if (userIndex === -1) {
+    return res.status(401).json({ message: "Incorrect username" })
+  }
+
+  if (users[userIndex].password === req.body.password) {
+    const payload = { id: users[userIndex].id }
+    const token = jwt.sign(payload, jwtOptions.secretOrKey)
+    return res.json({ token })
+  } else {
+    return res.status(401).json({ message: "Incorrect password" })
+  }
 })
 
 app.listen(3000, () => console.log("Server started on port 3000"))
